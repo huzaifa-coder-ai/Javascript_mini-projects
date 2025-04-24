67e8842e25b69ac15c106ae91bcbc8669dbf628a
Huzaifa Bilal <huzaifajanjua626@gmail.com>
2025-04-24 13:47:18 +0500

    Product Image Display Project added

---
+++ ProductDisplay_Js/app.js
@@ -0,0 +1,21 @@
+let productImg = document.getElementById("product-img")
+let button = document.getElementsByClassName("btn")
+
+button[0].onclick = function() {
+    productImg.src= "images/image3.png"
+    
+}
+button[1].onclick = function() {
+    productImg.src= "images/image2.png";
+    for(bt of button){
+        bt.classList.remove("active")
+    }
+    this.classList.add("active")
+}
+button[2].onclick = function() {
+    productImg.src= "images/image1.png";
+    for(bt of button){
+        bt.classList.remove("active")
+    }
+    this.classList.add("active")
+}
